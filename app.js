170bd9f4282e18d6c42e71fa74975485d424a257
Hacheur-1182 <bouviermaxime488@gmail.com>
2021-09-11 18:37:59 +0100

    add comments in app.js and routes.txt to describe how to create another route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// Import your route here
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -9,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }))
 
-
+// Add a reference on your route here
 app.use('/', indexRouter);
 
 module.exports = app;
