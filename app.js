167c32d3f2c8025cb009847f4c1bc6e59260ba67
Hacheur-1182 <bouviermaxime488@gmail.com>
2021-09-22 12:32:18 +0100

    configure morgan

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const logger = require('morgan');
 
 // Import your route here
 const indexRouter = require('./routes/index');
 
 const app = express();
 
+app.use(logger('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }))
